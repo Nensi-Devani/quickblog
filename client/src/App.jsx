a9a4648e5fa59d9d7bcc5c06bb6845caa2af7aca
Nensi Devani <ndevani894@rku.ac.in>
2025-06-10 00:34:36 +0530

    created pages and set-up routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Blog from "./pages/Blog"
+
 const App = () => {
   return (
     <>
-      <h1>App component</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/blog' element={<Blog />} />
+      </Routes>
     </>
   )
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <h2>Hoem apge</h2>
+    </>
+  )
+}
+
+export default Home
