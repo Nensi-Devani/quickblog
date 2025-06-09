768a3d2331422016a89eae3e9a552523b3207716
Nensi Devani <ndevani894@rku.ac.in>
2025-06-10 01:47:56 +0530

    created blog-categoty section and installed motion for animation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import BlogCategory from "../components/BlogCategory"
 import Header from "../components/Header"
 import Navbar from "../components/Navbar"
 
@@ -6,6 +7,7 @@ const Home = () => {
     <>
       <Navbar />
       <Header />
+      <BlogCategory />
     </>
   )
 }
