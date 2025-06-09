cbf0beb869760c9d526ca036148634dd52f664e5
Nensi Devani <ndevani894@rku.ac.in>
2025-06-10 02:08:53 +0530

    created blog-card

--- client/src/components/BlogCategory.jsx
+++ client/src/components/BlogCategory.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { motion } from "motion/react"
+import BlogCard from "./BlogCard"
 
 const BlogCategory = () => {
     const blogCategories = ['All', 'Technology', 'Startup', 'Lifestyle', 'Finance']
@@ -13,15 +14,20 @@ const BlogCategory = () => {
                         {category} 
                         {menu === category && (
                             <motion.div layoutId="underline"
-                            transition={{type:'spring', stiffness:500, damping:30 }} className="absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full"></motion.div>
+                            /* for animation */
+                            transition={{type:'spring', stiffness:500, damping:30 }}  className="absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full"></motion.div>
                         )}
                     </button>
                 </div>
             ))}
         </div> 
-        <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40">
             {/* Blog cards */}
-
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
         </div>
     </>
   )
