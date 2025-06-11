4af59827205877fd883ddcffe31db0a550043f2c
Nensi Devani <ndevani894@rku.ac.in>
2025-06-11 20:01:46 +0530

    update the login btn redirect path

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32">
       <NavLink to={'/'}> <img src={logo} alt="logo" /> </NavLink>
-      <NavLink to={'/admin'} className='flex items-center gap-2 rounded-full text-sm cursor-pointer   bg-primary text-white px-5 sm:px-10 py-2.5'>
+      <NavLink to={'/admin/'} className='flex items-center gap-2 rounded-full text-sm cursor-pointer   bg-primary text-white px-5 sm:px-10 py-2.5'>
         Login 
         <img src={arrow} alt="arrow" className='w-3' />
       </NavLink>
