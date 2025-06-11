c3a932ac4e005b6f3494540fe6d453c88ff0bd84
Nensi Devani <ndevani894@rku.ac.in>
2025-06-11 20:16:51 +0530

    add login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import Dashboard from "./pages/admin/Dashboard"
 import AddBlog from "./pages/admin/AddBlog"
 import Comments from "./pages/admin/Comments"
 import ListBlog from "./pages/admin/ListBlog"
+import Login from "./components/admin/Login"
+import 'quill/dist/quill.snow.css'
 
 const App = () => {
   return (
@@ -14,7 +16,7 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/blog/:id' element={<Blog />} />
         {/* admin routes */}
-        <Route path="/admin" element={<Layout />}>
+        <Route path="/admin" element={true ? <Layout /> : <Login />}>
           <Route index element={<Dashboard />} />
           <Route path="addBlog" element={<AddBlog />} />
           <Route path="listBlog" element={<ListBlog />} />
