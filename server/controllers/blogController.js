96fb35118ba86158c72c2e6bc0059844151d896f
Nensi Devani <ndevani894@rku.ac.in>
2025-06-13 16:22:19 +0530

    created function for fetch, fetch by id, delete and toggle status

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -34,8 +34,55 @@ export const createBlog = async (req, res) => {
 
         // store the data in mongodb
         const blog = await Blog.create({title, subTitle, description, category, image, isPublished})
-        res.json({success: true, message:'Blog added successbully', blog:blog})
+        res.json({success: true, message:'Blog added successfully', blog})
     }catch(error){
         res.json({success: false, message:`Error in adding the blog = ${error.message}`})
+    }
+}
+
+export const getAllBlogs = async (req, res) => {
+    try{
+        const blogs = await Blog.find({isPublished:true}) // return all the blogs with isPublished = true
+        res.json({success: true, blogs:blogs})
+    }catch(error){
+        res.json({success: false, message:`Error in fetching the blogs = ${error.message}`})
+    }
+}
+
+export const getBlogById = async (req, res) => {
+    try{
+        const {id} = req.params
+        const blog = await Blog.findById(id)
+        if(!blog)
+            res.json({success: false, message:`Blog not found !`})
+        res.json({success: true, blog})
+    }catch(error){
+        res.json({success: false, message:`Error in fetching the blog = ${error.message}`})
+    }
+}
+
+export const deleteBlog = async (req, res) => {
+    try{
+        const {id} = req.params
+        const blog = await Blog.findByIdAndDelete(id)
+        if(!blog)
+            res.json({success: false, message:`Blog Not Found !`})
+        res.json({success: true,message:`Blog deleted successfully`})
+    }catch(error){
+        res.json({success: false, message:`Error in fetching the blog = ${error.message}`})
+    }
+}
+
+export const togglePublish = async (req, res) => {
+    try{
+        const {id} = req.body
+        const blog = await Blog.findById(id)
+        if(!blog)
+            res.json({success: false, message:`Blog Not Found !`})
+        blog.isPublished = !blog.isPublished
+        await blog.save() // update the blog
+        res.json({success: true,message:`Blog status updated successfully`})
+    }catch(error){
+        res.json({success: false, message:`Error in updating the blog status = ${error.message}`})
     }
 }
