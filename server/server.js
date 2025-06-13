663d6fa3385a4088874bf4a43953a3360881ea10
Nensi Devani <ndevani894@rku.ac.in>
2025-06-13 18:28:27 +0530

    add comment route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import connectDB from './config/db.js'
 import adminRouter from './routes/adminRoutes.js'
 import blogRouter from './routes/blogRoutes.js'
+import commentRouter from './routes/commentRoutes.js'
 
 const app = express()
 await connectDB() // connect database
@@ -20,6 +21,8 @@ app.get('/', (req,res)=>{ // in browser, localhost:3000
 app.use('/api/admin', adminRouter)
 // blog route
 app.use('/api/blog', blogRouter)
+// comment route
+app.use('/api/comment', commentRouter)
 
 
 const PORT = process.env.PORT || 3000
