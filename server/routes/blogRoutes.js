5e87037216175fc6522d3e27a518cb23887be176
Nensi Devani <ndevani894@rku.ac.in>
2025-06-13 16:23:03 +0530

    add routes for fetch, fetch by id,delete and toggle status

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createBlog } from '../controllers/blogController.js'
+import { createBlog, deleteBlog, getAllBlogs, getBlogById, togglePublish } from '../controllers/blogController.js'
 import upload from '../middlewares/multer.js'
 import auth from '../middlewares/auth.js'
 
@@ -10,5 +10,9 @@ const blogRouter = express.Router()
 // uplaod = middleware for image upload
 // auth = middleware for authorized user access
 blogRouter.post('/', upload.single('image'), auth, createBlog) // url = /api/blog/
+blogRouter.get('/', getAllBlogs) // url = /api/blog/
+blogRouter.get('/:id', getBlogById) // url = /api/blog/blogId
+blogRouter.delete('/:id',auth, deleteBlog) // url = /api/blog/blogId  & only admin can delete the blog
+blogRouter.post('/toggle-publish',auth, togglePublish) // url = /api/blog/blogId
 
 export default blogRouter
