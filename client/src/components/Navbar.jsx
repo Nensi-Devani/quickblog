e816bb6f2404cdea9d3c279844f726230469fc0e
Nensi Devani <ndevani894@rku.ac.in>
2025-06-15 17:21:22 +0530

    add logic for login btn

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import logo from '../assets/logo.svg'
 import arrow from '../assets/arrow.svg'
 import { NavLink } from 'react-router-dom'
+import { useAppContext } from '../context/AppContext'
 
 const Navbar = () => {
+  const {token} = useAppContext()
+  
   return (
     <div className="flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32">
       <NavLink to={'/'}> <img src={logo} alt="logo" /> </NavLink>
       <NavLink to={'/admin/'} className='flex items-center gap-2 rounded-full text-sm cursor-pointer   bg-primary text-white px-5 sm:px-10 py-2.5'>
-        Login 
+        { token ? "Dashboard" : "Login"}
         <img src={arrow} alt="arrow" className='w-3' />
       </NavLink>
     </div>
